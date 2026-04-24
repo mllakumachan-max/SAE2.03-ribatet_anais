f6f0c5e08c0ec5cd74b01072eb48d0ac67d79b5f
Ribatet Anais <ribatet1@mmi-dev.unilim.fr>
2026-04-24 09:07:13 +0200

    update credentials

--- server/controller.php
+++ server/controller.php
@@ -1,112 +1,68 @@
 <?php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-/** ARCHITECTURE PHP SERVEUR : Rôle du fichier script.php
+/** ARCHITECTURE PHP SERVEUR  : Rôle du fichier controller.php
  * 
- * Ce fichier est celui à qui on adresse toutes les requêtes HTTP.
- * Pour être valide, on décide que chaque requête doit contenir un paramètre 'todo'.
- * C'est un choix d'implémentation, on aurait pu choisir un autre nom de paramètre.
- * L'interprétation de la requête se fait en fonction de la valeur du paramètre 'todo'.
- * Selon cette valeur, on fait appelle à la fonction de contrôleur (voir controller.php)
- * appropriée pour traiter la requête HTTP et produire la réponse HTTP attendue..
+ *  Dans ce fichier, on va définir les fonctions de contrôle qui vont traiter les requêtes HTTP.
+ *  Les requêtes HTTP sont interprétées selon la valeur du paramètre 'todo' de la requête (voir script.php)
+ *  Pour chaque valeur différente, on déclarera une fonction de contrôle différente.
  * 
- * Pourquoi faire comme ça ?
- * 
- *  En ajoutant un paramètre 'todo' dans la requête, on a un seul paramètre à regarder pour déterminer l'action à effectuer.
- *  Sinon il faudrait toujours analyser tous les paramètres de la requête pour déterminer l'action à effectuer.
- *  Et dans une véritable application il peut y avoir énormément de paramètres, ce qui deviendrait compliqué et illisible.
- * 
+ *  Les fonctions de contrôle vont éventuellement lire les paramètres additionnels de la requête, 
+ *  les vérifier, puis appeler les fonctions du modèle (model.php) pour effectuer les opérations
+ *  nécessaires sur la base de données.
+ *  
+ *  Si la fonction échoue à traiter la requête, elle retourne false (mauvais paramètres, erreur de connexion à la BDD, etc.)
+ *  Sinon elle retourne le résultat de l'opération (des données ou un message) à inclure dans la réponse HTTP.
  */
 
-/**
- * Inclusion du fichier controller.php.
+/** Inclusion du fichier model.php
+ *  Pour pouvoir utiliser les fonctions qui y sont déclarées et qui permettent
+ *  de faire des opérations sur les données stockées en base de données.
+ */
+require("model.php");
+
+
+function readMoviesController(){
+    $movies = getAllMovies();
+    return $movies;
+}
+
+/** addMovieController
  * 
- * Il contient les fonctions nécessaires pour traiter chaque type de requête
- * et définir la réponse à renvoyer au client.
- */
-require("controller.php");
-
-/**
- * Vérifie si la variable 'todo' est définie dans la requête.
+ * Cette fonction est en charge du traitement des requêtes HTTP pour lesquelles le paramètre 'todo' vaut 'add'.
+ * Elle vérifie si le paramètre titre est défini et non vide dans la requête.
+ * Si le paramètre 'titre' est présent, elle appelle la fonction addMovie et ajoute le film.
+ * Si le paramètre 'titre' n'est pas présent, vide ou invalide, elle retourne false.
  * 
- * Cette condition permet de déterminer si un paramètre 'todo' a été envoyé
- * via une requête HTTP. 
- * Si ce paramètre est présent, le code à l'intérieur du bloc conditionnel sera exécuté.
+ * @return mixed Le film si 'titre' est défini, valide et non vide, sinon false.
  */
-if ( isset($_REQUEST['todo']) ){
-
-  /**
-   * La fonction PHP header permet de définir l'en-tête HTTP de la réponse.
-   * 
-   * A ce stade on sait qu'on va répondre à la requête HTTP avec du contenu JSON (même pour signaler une erreur).
-   * Donc on définit de suite l'en-tête de la réponse HTTP pour indiquer que le contenu sera de type JSON.
-   * Ce n'est pas obligatoire, mais c'est une bonne pratique pour indiquer clairement le type de contenu.
-   * Le client à qui on répond pourra tester le type de contenu de la réponse pour mieux comprendre et 
-   * traiter la réponse du serveur.
-   */
-  header('Content-Type: application/json');
-
-  // Récupère la valeur du paramètre 'todo' dans le tableau $_REQUEST
-  // $_REQUEST est une superglobale qui contient les paramètres de la requête HTTP.
-  $todo = $_REQUEST['todo'];
-
-  // en fonction de la valeur de 'todo', on appelle la fonction de contrôle appropriée
-  // peut s'écrire aussi avec des if/else
-  switch($todo){
-
-    case 'readmovies':
-      $data = readMoviesController();
-      break;
-
-    case 'addmovie':
-      $data = addMovieController();
-      break;
-
-    default: // il y a un paramètre todo mais sa valeur n'est pas reconnue/supportée
-      echo json_encode('[error] Unknown todo value');
-      http_response_code(400); // 400 == "Bad request"
-      exit();
-  }
-
-  /**
-   * A ce stade, on a appelé la fonction de contrôleur appropriée et stocké le résultat dans la variable $data.
-   * 
-   * On a décidé que si les fonctions de contrôleur échouaient à répondre normalement à la requête HTTP,
-   * elle devait retourner false (par exemple il peut arriver que le serveur de base de données soit down).
-   * C'est un choix qui nous permet de savoir si un problème est survenu et de retourner un message d'erreur.
-   * Si la fonction de contrôleur retourne false, on renvoie une réponse JSON avec un message d'erreur 
-   * et un code de réponse HTTP 500 (Internal error), puis termine l'exécution du script (exit()).
-   */
-  if ($data===false){
-    echo json_encode('[error] Controller returns false');
-    http_response_code(500); // 500 == "Internal error"
-    exit();
-  }
-
-  /**
-   * Si tout s'est bien passé, on renvoie la réponse HTTP avec les données ($data) retournées
-   * par la fonction de contrôleur et encodées en JSON (json_encode).
-   * On renvoie aussi un code de réponse HTTP 200 (OK) pour indiquer que la requête a été traitée avec succès.
-   */
-  echo json_encode($data);
-  http_response_code(200); // 200 == "OK"
-  exit();
-
-   
-} // fin de if ( isset($_REQUEST['todo']) )
-
-
-/**
- * Cette partie du code n'est atteinte que si la variable 'todo' n'est pas définie dans la requête.
- * La conception de notre script est basée sur le fait qu'une variable todo doit être définie. Dans
- * le cas contraire, on considère que la requête est invalide et on renvoie un code de réponse 
- * HTTP 404 (Not found), indiquant que la requête HTTP ne correspond à rien.
- */
-http_response_code(404); // 404 == "Not found"
-
-
-
-?>
+function addMovieController(){
+    // Lecture des données de formulaire
+    $titre = $_REQUEST['titre'];
+    $annee = $_REQUEST['annee'];
+    $duree = $_REQUEST['duree'];
+    $desc = $_REQUEST['desc'];
+    $real = $_REQUEST['real'];
+    $categorie = $_REQUEST['categorie'];
+    $img = $_REQUEST['img'];
+    $lien = $_REQUEST['lien'];
+    $age = $_REQUEST['age'];
+    // Vérifie que le paramètre 'titre' n'est pas absent ou vide
+    if (isset($titre)==true && empty($titre)==false){
+        // Ajout du film à l'aide de la fonction addMovie décrite dans model.php
+        addMovie($titre, $annee, $duree, $desc, $real, $categorie, $img, $lien, $age);
+        // Vérifie que le paramètre 'age' n'est pas absent ou vide
+        if (isset($age)==true && empty($age)==false){
+            $min_age = [10, 12, 16, 18];
+            if (in_array($age, $min_age)){
+                return "Le film $titre a été ajouté avec succès.";
+            }
+            else{
+                return false;
+            }
+        }
+        return "Le film $titre a été ajouté avec succès.";
+    }
+    else{
+        return false;
+    }
+}
